40e1b03d16913c8fefaf36d0a0ba24853ff2d67c
Eric D. Schabell <eric@schabell.org>
2010-12-08 10:13:48 +0000

    Adding in start with the java object for mapping the process definition of jpdl.

---
+++ src/main/java/org/jbpm/model/ProcessDefinition.java
@@ -0,0 +1,49 @@
+/**
+ * 
+ */
+package org.jbpm.model;
+
+import java.util.ArrayList;
+
+/**
+ * Class to mapping jPDL Process Definition types to java object.
+ * 
+ * @author Eric D. Schabell
+ *
+ */
+public class ProcessDefinition {
+	
+	// Complex type that can be a description, swimlane, start-state, 
+	// node-elements list, action-elemenst list, event, exception-handler 
+	// or task.
+	private String description;
+	private String xmlns;
+// TODO:    private Swimlane swimlane;;
+// TODO:	private StartState startState;
+	private String name;
+    
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setXmlns(String xmlns) {
+		this.xmlns = xmlns;
+	}
+
+	public String getXmlns() {
+		return xmlns;
+	}
+    
+}
