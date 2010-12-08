d01bc09cc061afe3232a8abae4290d3d2d4dbdd7
Eric D. Schabell <eric@schabell.org>
2010-12-08 09:54:20 +0000

    Extended Node mapping class a bit, marked todo for types not yet available.

--- src/main/java/org/jbpm/model/Node.java
+++ src/main/java/org/jbpm/model/Node.java
@@ -3,20 +3,34 @@
  */
 package org.jbpm.model;
 
-import java.util.ArrayList;
 
 /**
+ * Class to mapping jPDL Node types to java object.
+ *  
  * @author eschabel
  *
  */
 public final class Node {
 	
-	private String name = "node";
+	private String name;
 	private String async = "false";
-	private String actionElements;
-	private ArrayList<String> nodeContentElements;
+	
+	// action-elements
+	private String actionClass;
+	private String actionConfigType;
+    private String actionName;
+    private String actionRefName;
+    private Boolean actionAcceptPropagatedEvents = true;   // default value is true.
+    private String actionExpression;
+    private String actionAsyc;
 
-
+    // node-content-elements
+    private String nodeContentDescription;
+// TODO:    private Event nodeContentEvent;
+// TODO:    private ExceptionHandler nodeContentExceptionHandler;
+// TODO:    private Timer nodeContentTimer;
+// TODO:    private Transition nodeContentTransition;
+    
 	public void setName(String name) {
 		this.name = name;
 	}
@@ -33,20 +47,70 @@ public final class Node {
 		return async;
 	}
 
-	public void setActionElements(String actionElements) {
-		this.actionElements = actionElements;
+	public void setActionClass(String actionClass) {
+		this.actionClass = actionClass;
 	}
 
-	public String getActionElements() {
-		return actionElements;
+	public String getActionClass() {
+		return actionClass;
 	}
 
-	public void setNodeContentElements(ArrayList<String> nodeContentElements) {
-		this.nodeContentElements = nodeContentElements;
+	public void setActionConfigType(String actionConfigType) {
+		this.actionConfigType = actionConfigType;
 	}
 
-	public ArrayList<String> getNodeContentElements() {
-		return nodeContentElements;
+	public String getActionConfigType() {
+		return actionConfigType;
 	}
+
+	public void setActionName(String actionName) {
+		this.actionName = actionName;
+	}
+
+	public String getActionName() {
+		return actionName;
+	}
+
+	public void setActionRefName(String actionRefName) {
+		this.actionRefName = actionRefName;
+	}
+
+	public String getActionRefName() {
+		return actionRefName;
+	}
+
+	public void setActionAcceptPropagatedEvents(
+			Boolean actionAcceptPropagatedEvents) {
+		this.actionAcceptPropagatedEvents = actionAcceptPropagatedEvents;
+	}
+
+	public Boolean getActionAcceptPropagatedEvents() {
+		return actionAcceptPropagatedEvents;
+	}
+
+	public void setActionExpression(String actionExpression) {
+		this.actionExpression = actionExpression;
+	}
+
+	public String getActionExpression() {
+		return actionExpression;
+	}
+
+	public void setActionAsyc(String actionAsyc) {
+		this.actionAsyc = actionAsyc;
+	}
+
+	public String getActionAsyc() {
+		return actionAsyc;
+	}
+
+	public void setNodeContentDescription(String nodeContentDescription) {
+		this.nodeContentDescription = nodeContentDescription;
+	}
+
+	public String getNodeContentDescription() {
+		return nodeContentDescription;
+	}
+
 	
 }
