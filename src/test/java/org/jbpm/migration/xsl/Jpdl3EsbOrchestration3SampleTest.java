433f9aa1d01250aa0e2a3868edae29293b3ec35f
Eric D. Schabell <eric@schabell.org>
2011-04-18 21:18:19 +0200

    Added proc def for orchestration3 sample quickstart process, unit test created and conversion working fine. Closes gh-25.

---
+++ src/test/java/org/jbpm/migration/xsl/Jpdl3EsbOrchestration3SampleTest.java
@@ -0,0 +1,101 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2005, JBoss Inc., and individual contributors as indicated
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.jbpm.migration.xsl;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.assertThat;
+
+import java.io.File;
+
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.jbpm.migration.util.XmlUtils;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.w3c.dom.Document;
+
+/**
+ * Tests for the jPDL process definition transformer with JAXP.
+ * 
+ * @author Eric D. Schabell
+ * @author Maurice de Chateau
+ */
+public class Jpdl3EsbOrchestration3SampleTest {
+
+    // input jpdl file.
+    private static final String INPUT_JPDL = "src/test/resources/jpdl3/esbOrchestration3Sample/processdefinition.xml";
+
+    // xsd sheet.
+    private static final String XSD_SHEET = "src/main/resources/jpdl3-bpmn2.xsl";
+
+    // results file of transformation.
+    private static final String RESULTS_FILE = "target/processdefinition.bpmn.xml";
+
+    @BeforeClass
+    public static void oneTimeSetUp() {
+        BasicConfigurator.configure();
+        Logger.getRootLogger().setLevel(Level.ERROR);
+    }
+
+    /**
+     * No Validation of the jPDL input file, it is not valid!
+     * 
+     * @throws Exception (every time!)
+     */
+//    @Test
+//    public void validJpldDefinition() throws Exception {
+//        File jpdl = new File(INPUT_JPDL);
+//        try {
+//            Document document = JpdlValidator.validateDefinition(jpdl);
+//            assertThat(document, is(notNullValue()));
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//            fail("Not a valid jPDL definition.");
+//        }
+//    }
+
+    /**
+     * Transform the input file.
+     * 
+     * @throws Exception
+     */
+    @Test
+    public void transformjPDL() throws Exception {
+        SimpleJaxpTransformer.main(new String[] { INPUT_JPDL, XSD_SHEET, RESULTS_FILE });
+        assertThat(RESULTS_FILE, is(notNullValue()));
+    }
+
+    /**
+     * Validate the BPMN output file.
+     * 
+     * @throws Exception
+     */
+    @Test
+    public void validBpmnDefinition() throws Exception {
+        File bpmn = new File(RESULTS_FILE);
+        Document bpmnDoc = XmlUtils.parseFile(bpmn);
+        assertThat(bpmnDoc, is(notNullValue()));
+        assertThat(BpmnValidator.validateDefinition(bpmnDoc), is(true));
+    }
+}
